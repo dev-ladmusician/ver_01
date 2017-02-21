d1f26791f567176da755535a70974dd7d46415bd
HanWool Lee <hanwool@goqual.com>
2017-02-21 14:09:35 +0900

    Revert "Feature remote"

--- app/src/main/java/com/goqual/a10k/view/adapters/interfaces/OnRecyclerItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.goqual.a10k.view.adapters.interfaces;
-
-/**
- * Created by ladmusician on 2017. 2. 20..
- */
-
-public interface OnRecyclerItemClickListener {
-    void onItemClick(int viewId, int position);
-}
